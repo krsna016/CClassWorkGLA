a83ca924794062e5bac63af65ed0d6436ac51b3c
krsna016 <016.krsna@gmail.com>
2022-11-01 17:49:40 +0530

    Added new files

---
+++ C_program_69.c
@@ -0,0 +1,17 @@
+/*
+ * Write a C program to input any character and if its uppercase alphabet 
+ * then convert it into lowercase alphabet and vice versa using switch case ?
+ * Student name : Anurag pareek
+ * Sec : J
+ */
+#include <stdio.h>
+int main()
+{
+    char character;
+    int expression;
+    printf("Enter the character : "); 
+    scanf("%c", &character); // Let,character = 'A'
+    expression = 32+character;
+    printf("%d\n", expression);
+    return 0;
+}
---
+++ C_program_72.c
@@ -0,0 +1,15 @@
+/*
+ * Write a program to calculate age of a
+ * person based on his given date of birth ?
+ * Student name : Anurag pareek
+ * Sec : J
+ */
+#include <stdio.h>
+int main()
+{
+    int dob;
+    printf("Enter the date of birth in the format (DDMMYYYY) : \n");
+    scanf("%d%d%d", &dob,&dob,&dob);
+    printf("Your date of birth is : %d\n",dob);
+    return 0;
+}
